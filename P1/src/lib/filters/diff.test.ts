8d4c9791560fd99af0fc35b2188bbc4f1680f862
stephenarthur64 <148497387+stephenarthur64@users.noreply.github.com>
2026-09-14 11:20:31 +0100

    Review comment fixed

--- P1/src/lib/filters/diff.test.ts
+++ P1/src/lib/filters/diff.test.ts
@@ -12,7 +12,7 @@ describe('Diff filter 1', () => {
 describe('Diff filter 2', () => {
     it('finds absolute difference between current and previous value', () => {
         const input = [4, 2, 8, 3, 9, 4, 10, 5, 11, 6];
-        const expected = [4, 2, 3, 5, 6, 5, 6, 5, 6, 5];
+        const expected = [4, 2, 6, 5, 6, 5, 6, 5, 6, 5];
         expect(diffs(input)).toEqual(expected);
     });
 });
